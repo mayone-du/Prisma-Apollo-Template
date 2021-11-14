302b4bd7acda7c3ab258d679f8dc45f21c81610b
mayone-du <cocomayo1201@icloud.com>
2021-11-14 16:38:15 +0900

    Add npm scripts, fix tsconfig, migrate

---
+++ prisma/migrations/20211114073019_init/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "isAdmin" BOOLEAN NOT NULL DEFAULT false,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "intro" TEXT NOT NULL,
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_userId_key" ON "Profile"("userId");
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,24 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const main = async () => {
-  // ... you will write your Prisma Client queries here
+  await prisma.user.create({
+    data: {
+      name: "Alice",
+      email: "alice@prisma.io",
+      isActive: true,
+      isAdmin: false,
+      profile: {
+        create: { intro: "I like turtles" },
+      },
+    },
+  });
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      profile: true,
+    },
+  });
+  console.dir(allUsers, { depth: null });
 };
 
 main()
