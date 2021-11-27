d1603578bab953d8cef97620eabf9767d142949c
mayone-du <cocomayo1201@icloud.com>
2021-11-27 17:35:21 +0900

    Add mutation

---
+++ src/schemas/mutation/user.ts
@@ -0,0 +1,51 @@
+import { arg, extendType, inputObjectType, nonNull } from "nexus";
+
+import { userObject } from "../";
+
+const createUserInput = inputObjectType({
+  name: "CreateUserInput",
+  definition(t) {
+    t.nonNull.string("email");
+  },
+});
+
+const updateUserInput = inputObjectType({
+  name: "UpdateUserInput",
+  definition(t) {
+    t.nonNull.string("email");
+  },
+});
+
+export const userMutation = extendType({
+  type: "Mutation",
+  definition(t) {
+    // create
+    t.field("createUser", {
+      type: userObject,
+      args: { input: nonNull(arg({ type: createUserInput })) },
+      resolve: async (_root, args, ctx) => {
+        await ctx.prisma.user.create({
+          data: {
+            ...args.input,
+          },
+        });
+      },
+    });
+
+    // update
+    t.field("updateUser", {
+      type: userObject,
+      args: { input: nonNull(arg({ type: updateUserInput })) },
+      resolve: async (_root, args, ctx) => {
+        await ctx.prisma.user.update({
+          where: {
+            email: args.input.email,
+          },
+          data: {
+            ...args.input,
+          },
+        });
+      },
+    });
+  },
+});
